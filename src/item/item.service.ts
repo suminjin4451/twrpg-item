f8740b0838e3cb38149526de85215308176f32b0
KimMalsu <jsm4451@naver.com>
2022-05-30 16:15:17 +0900

    item List, css update

--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -9,6 +9,32 @@ export class ItemService {
     itemNameArray: Array<string>,
     itemArray: Array<[string, number, string]>,
   ) {
+    const bossSequence = [
+      '킹콩',
+      '바다 코끼리',
+      '마법사 왕',
+      '잭 오 랜턴',
+      '블러드 체페슈',
+      '하이드라',
+      '능천사',
+      '타천사',
+      '마나 에인션트',
+      '매드 클라운',
+      '서리한, 일셰나',
+      '거미제왕',
+      '서리한, 일셰나, 거미제왕',
+      '마왕',
+      '커럽터 렉터스',
+      '플레임 나이트메어',
+      '터틀 로드',
+      '스피릿 비스트',
+      '해골왕 데스페리아',
+      '주천사 사미엘',
+      '좀비 로드',
+      '본 드래곤',
+      '에인션트 엔트',
+      '암흑룡 이르베르트',
+    ];
     for (let itemName of itemNameArray) {
       if (Items[itemName] == undefined) {
         this.logger.log(Items[itemName]);
@@ -34,6 +60,15 @@ export class ItemService {
         }
       }
     }
+
+    itemArray.sort(function (a, b) {
+      console.log(a + ' ' + b);
+      if (bossSequence.indexOf(a[2]) > bossSequence.indexOf(b[2])) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
     return itemArray;
   }
 }
